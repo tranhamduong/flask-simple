14cceab590bee417fb11e358ce98743ca0f6a365
tranhamduong <tranhamduong@gmail.com>
2021-04-05 00:14:34 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ from app import app
 port = int(os.getenv("PORT", 0))
 if __name__ == '__main__':
     if port != 0:
-        app.run(host='0.0.0.0', port=port)
+        app.run(host='0.0.0.0', port=8000)
     else:
         app.run()
